a1f55662a5327230dd1dd183cc0c09195c220969
philip <philip@fermee.com>
2020-10-12 15:32:44 -0500

    working copy: made Matrix and SetMatrix on Grid level this one just reacts on clicks and flips color (including another cell just to show it's possible)

--- src/Grid.js
+++ src/Grid.js
@@ -1,40 +1,54 @@
-import React, { useState, setState } from "react";
+import React, { useState, useCallback } from "react";
 
 function Cell(props) {
   // color of cell depends on the className (so that's the state of "this" brick)
-  const [style, setStyle] = useState("blue");
+  //const [style, setStyle] = useState("blue");
+
+  // now we have props.theMatrix..
+
+  //console.log(`in Cell: ${props.rowid - 1} ${props.cellid - 1}`);
+
+  let color = props.theMatrix[props.rowid][props.cellid];
+  // console.log(`ROW: ${props.rowid} COL:${props.cellid}: color ${color}`);
 
   //  console.log(props.id + ":" + style);
   return (
     <div
       // onChildClick is defindd on the App level (as a start to lift up style to a matrix)
       // big question is this the right approach?
-      onClick={(x) => props.onCellClick(style, setStyle)}
-      className={style}
+      onClick={(x) => props.onCellClick(props.rowid, props.cellid)}
+      className={color}
       id={props.id}
-      key={props.key}
+      rowid={props.rowid}
+      cellid={props.cellid}
     />
   );
 }
 
 // Create a row of "cells" cells
 function GridRow(props) {
-  var cells = [],
-    c = 0;
+  var cells = [];
 
   // create cells with  unique cells (id:row-col)
-  while (++c <= props.cells) {
+  for (let c = 0; c < props.cells; c++) {
     let id = props.row + "-" + c;
-    cells.push(id);
+    let cellid = c;
+    let index = [id, cellid];
+    cells.push(index);
   }
-
-  cells.map((id) => id);
 
   return (
     <div className="grid-row" id={props.row} key={props.row}>
-      {cells.map((xy) => (
-        <Cell id={xy} key={xy} onCellClick={props.onCellClick}>
-          {xy}
+      {cells.map((idx) => (
+        <Cell
+          id={idx[0]}
+          key={idx[0]}
+          rowid={props.row}
+          cellid={idx[1]}
+          onCellClick={props.onCellClick}
+          theMatrix={props.theMatrix}
+        >
+          {idx[0]}
         </Cell>
       ))}
     </div>
@@ -43,24 +57,55 @@ function GridRow(props) {
 
 // Create a grid of "#rows" rows... * #cells cells
 export default function Grid(props) {
+  // this is a callback for all the cells (triggered from onClick on a cell)...
+  // trying to 'lift up state! (and change single cell to matrix)'
+  function onCellClicked(row, cell) {
+    // create a clone!
+    let newMatrix = matrix.map((x) => x);
+
+    console.log(`callback BEFORE: ${newMatrix[row][cell]}`);
+
+    if (newMatrix[row][cell][0] === "red") {
+      // not quite sure why [0] is needed here...
+      // i can see that n[x][y] is ["blue"], i'm guessing i am 1 level too deep
+      newMatrix[row][cell] = ["blue"];
+    } else {
+      newMatrix[row][cell] = ["red"];
+      if (row >= 1 && cell >= 1) {
+        newMatrix[row - 1][cell - 1] = ["red"];
+      }
+    }
+
+    console.log(`callback AFTER: ${newMatrix[row][cell]}`);
+
+    setMatrix(newMatrix);
+  }
+
+  const cbCellClicked = useCallback(
+    (row, cell) => onCellClicked(row, cell),
+    []
+  );
+
   let rows = [],
-    r = 0,
-    len = props.rows;
+    r = 0;
 
   let initMatrix = [];
 
   // create rows and start 'the matrix'
-  while (++r <= len) {
+  while (r < props.rows) {
     rows.push(r);
     initMatrix.push([]);
-    while (++c <= props.cells) {
+    let c = 0;
+    while (c < props.cells) {
       // add cellsA
-      console.log("process row" + r);
-      initMatrix[r].push([blue]);
+      //      console.log("process row" + r);
+      initMatrix[r].push(["blue"]);
+      c++;
     }
+    r++;
   }
 
-  [matrix, setMatrix] = useState([initMatrix]);
+  const [matrix, setMatrix] = useState(initMatrix);
 
   return (
     <div className="brick-grid" id={props.id} key={props.id}>
@@ -69,9 +114,10 @@ export default function Grid(props) {
           className="grid-row"
           key={i}
           row={i}
-          onCellClick={props.onCellClick}
+          onCellClick={cbCellClicked}
           cells={props.cells}
           cellStyle={props.cellStyle}
+          theMatrix={matrix}
         ></GridRow>
       ))}
     </div>
