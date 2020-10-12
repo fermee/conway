f9088e2ec3cc3c6d7b183c5fc7de2122985d2e1e
philip <philip@fermee.com>
2020-10-12 18:34:52 -0500

    updates: up/down/left/right and click to reset position all working but a bit slow

--- src/Grid.js
+++ src/Grid.js
@@ -1,27 +1,23 @@
-import React, { useState, useCallback } from "react";
+import React, { useRef, useEffect, useState, useCallback } from "react";
 
 function Cell(props) {
-  // color of cell depends on the className (so that's the state of "this" brick)
-  //const [style, setStyle] = useState("blue");
-
-  // now we have props.theMatrix..
-
-  //console.log(`in Cell: ${props.rowid - 1} ${props.cellid - 1}`);
-
   let color = props.theMatrix[props.rowid][props.cellid];
   // console.log(`ROW: ${props.rowid} COL:${props.cellid}: color ${color}`);
+  if (props.pos[0] === props.rowid && props.cellid === props.pos[1]) {
+    color = "green";
+  }
 
-  //  console.log(props.id + ":" + style);
   return (
     <div
-      // onChildClick is defindd on the App level (as a start to lift up style to a matrix)
-      // big question is this the right approach?
       onClick={(x) => props.onCellClick(props.rowid, props.cellid)}
       className={color}
       id={props.id}
+      key={props.id}
       rowid={props.rowid}
       cellid={props.cellid}
-    />
+    >
+      {props.children}
+    </div>
   );
 }
 
@@ -47,6 +43,7 @@ function GridRow(props) {
           cellid={idx[1]}
           onCellClick={props.onCellClick}
           theMatrix={props.theMatrix}
+          pos={props.pos}
         >
           {idx[0]}
         </Cell>
@@ -55,28 +52,51 @@ function GridRow(props) {
   );
 }
 
+function InitMatrix(rows, cols, style) {
+  // style is a CSS class, typically "red", "blue"
+
+  let initMatrix = [];
+
+  // create rows and start 'the matrix'
+  for (let r = 0; r < rows; r++) {
+    initMatrix.push([]);
+    for (let c = 0; c < cols; c++) {
+      // inital color=blue
+      initMatrix[r].push(style);
+    }
+  }
+  return initMatrix;
+}
+
+function ZeroInit(clmat, rows, cols) {
+  for (let i = 0; i < rows; i++) {
+    for (let j = 0; j < cols; j++) {
+      clmat[i][j] = "blue";
+    }
+  }
+  return clmat;
+}
 // Create a grid of "#rows" rows... * #cells cells
 export default function Grid(props) {
   // this is a callback for all the cells (triggered from onClick on a cell)...
-  // trying to 'lift up state! (and change single cell to matrix)'
   function onCellClicked(row, cell) {
-    // create a clone!
-    let newMatrix = matrix.map((x) => x);
+    // create a clone (don't forget this or render won't work and you're changing original state)!
+    let newMatrix = [...matrix];
 
-    console.log(`callback BEFORE: ${newMatrix[row][cell]}`);
+    newMatrix[pos[0]][pos[1]] = "blue";
+    let newpos = pos;
 
-    if (newMatrix[row][cell][0] === "red") {
-      // not quite sure why [0] is needed here...
-      // i can see that n[x][y] is ["blue"], i'm guessing i am 1 level too deep
-      newMatrix[row][cell] = ["blue"];
-    } else {
-      newMatrix[row][cell] = ["red"];
-      if (row >= 1 && cell >= 1) {
-        newMatrix[row - 1][cell - 1] = ["red"];
-      }
-    }
+    // newpos=[row,cell] // does not fully work,
+    // a render from another event resets the state to the previous position!
+    newpos[0] = row;
+    newpos[1] = cell;
+    setPos(newpos);
 
-    console.log(`callback AFTER: ${newMatrix[row][cell]}`);
+    /*
+    newMatrix[row][cell] === "red"
+      ? (newMatrix[row][cell] = "blue")
+      : (newMatrix[row][cell] = "red");
+    */
 
     setMatrix(newMatrix);
   }
@@ -86,38 +106,114 @@ export default function Grid(props) {
     []
   );
 
-  let rows = [],
-    r = 0;
+  const Pressing = useKeyPress("h", ClearMatrix);
+  const GoUp = useKeyPress("i", PosUp);
+  const GoDown = useKeyPress("m", PosDown);
+  const GoLeft = useKeyPress("j", PosLeft);
+  const GoRight = useKeyPress("k", PosRight);
 
-  let initMatrix = [];
+  function ClearMatrix() {
+    console.log("state in ClearMatrix event handler:");
+    let clmat = [...matrix];
+    clmat = ZeroInit(clmat, matrix.length, matrix[0].length);
 
-  // create rows and start 'the matrix'
-  while (r < props.rows) {
-    rows.push(r);
-    initMatrix.push([]);
-    let c = 0;
-    while (c < props.cells) {
-      // add cellsA
-      //      console.log("process row" + r);
-      initMatrix[r].push(["blue"]);
-      c++;
-    }
-    r++;
+    setMatrix(clmat);
   }
 
-  const [matrix, setMatrix] = useState(initMatrix);
+  function useKeyPress(key, action) {
+    useEffect(() => {
+      function onKeyup(e) {
+        if (e.key === key) action();
+      }
+      window.addEventListener("keyup", onKeyup);
+      return () => window.removeEventListener("keyup", onKeyup); // remove listener before re-adding?
+    }, []);
+  }
+
+  function PosUp() {
+    let oldpos = pos;
+    let newmatrix = [...matrix];
+
+    newmatrix[oldpos[0]][oldpos[1]] = "red";
+    oldpos[0] = oldpos[0] > 0 ? oldpos[0] - 1 : oldpos[0];
+    setPos(oldpos);
+    newmatrix[oldpos[0]][oldpos[1]] = "green";
+    setMatrix(newmatrix);
+  }
+
+  function PosDown() {
+    let oldpos = pos;
+    let newmatrix = [...matrix];
+
+    newmatrix[oldpos[0]][oldpos[1]] = "red";
+    oldpos[0] = oldpos[0] < matrix.length - 1 ? oldpos[0] + 1 : oldpos[0];
+    setPos(oldpos);
+    newmatrix[oldpos[0]][oldpos[1]] = "green";
+    setMatrix(newmatrix);
+  }
+
+  function PosLeft() {
+    let oldpos = pos;
+    let newmatrix = [...matrix];
+    newmatrix[oldpos[0]][oldpos[1]] = "red";
+    oldpos[1] = oldpos[1] > 0 ? oldpos[1] - 1 : oldpos[1];
+    setPos(oldpos);
+    newmatrix[oldpos[0]][oldpos[1]] = "green";
+    setMatrix(newmatrix);
+  }
+
+  function PosRight() {
+    let oldpos = pos;
+    let newmatrix = [...matrix];
+
+    newmatrix[oldpos[0]][oldpos[1]] = "red";
+    oldpos[1] = oldpos[1] < 49 ? oldpos[1] + 1 : oldpos[1];
+    setPos(oldpos);
+    newmatrix[oldpos[0]][oldpos[1]] = "green";
+    setMatrix(newmatrix);
+  }
+
+  function useTraceUpdate(props) {
+    const prev = useRef(props);
+    useEffect(() => {
+      const changedProps = Object.entries(props).reduce((ps, [k, v]) => {
+        if (prev.current[k] !== v) {
+          ps[k] = [prev.current[k], v];
+        }
+        return ps;
+      }, {});
+      if (Object.keys(changedProps).length > 0) {
+        console.log("Changed props:", changedProps);
+      }
+      prev.current = props;
+    });
+  }
+
+  let rows = [];
+
+  // create rows and start 'the matrix'
+  for (let r = 0; r < props.rows; r++) {
+    rows.push(r);
+  }
+
+  const [matrix, setMatrix] = useState(
+    InitMatrix(props.rows, props.cells, props.cellStyle)
+  );
+
+  const [pos, setPos] = useState([10, 10]);
 
   return (
     <div className="brick-grid" id={props.id} key={props.id}>
       {rows.map((i) => (
         <GridRow
           className="grid-row"
+          id={i}
           key={i}
           row={i}
           onCellClick={cbCellClicked}
           cells={props.cells}
-          cellStyle={props.cellStyle}
           theMatrix={matrix}
+          pos={pos}
         ></GridRow>
       ))}
     </div>
